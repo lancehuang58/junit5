50a67e9361bf07301f48ef9b8f811896268858a2
Lance Huang <lance.huang@deltaww.com>
2022-09-23 15:55:19 +0800

    init

--- src/main/java/idv/lance/example/Api.java
+++ src/main/java/idv/lance/example/Api.java
@@ -2,15 +2,19 @@ package idv.lance.example;
 
 public class Api {
 
-    public boolean validate() {
-        return true;
-    }
+  public boolean validate() {
+    return true;
+  }
 
-    public void transfer() {
-        throw new RuntimeException();
-    }
+  public void transfer() {
+    throw new RuntimeException();
+  }
 
-    public void commit() throws Exception {
-        throw new Exception("");
-    }
+  public String getName() {
+    return Api.class.getSimpleName();
+  }
+
+  public void commit() throws Exception {
+    throw new Exception("");
+  }
 }
--- src/test/java/idv/lance/example/ApiTest.java
+++ src/test/java/idv/lance/example/ApiTest.java
@@ -1,41 +1,47 @@
 package idv.lance.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApiTest {
-    Api api = new Api();
+  Api api = new Api();
 
-    @BeforeEach
-    void init() {
+  @BeforeEach
+  void init() {}
 
-    }
+  @Test
+  @DisplayName("test case for assert true ")
+  void first_test_assert_true() {
+    assertTrue(api.validate());
+  }
 
-    @Test
-    @DisplayName("test case for assert true ")
-    void first_test_assert_true() {
-        assertTrue(api.validate());
-    }
+  @Tag("exception")
+  @Test
+  @DisplayName("test case for test throw descendant exception")
+  void test_descendant_exception_being_throw() {
+    assertThrows(RuntimeException.class, () -> api.transfer());
+  }
 
-    @Tag("exception")
-    @Test
-    @DisplayName("test case for test throw descendant exception")
-    void test_descendant_exception_being_throw() {
-        assertThrows(
-                RuntimeException.class,
-                () -> api.transfer());
-    }
+  @Tag("exception")
+  @Test
+  @DisplayName("test case for test throw exact exception")
+  void test_exception_exactly() {
+    assertThrows(Exception.class, () -> api.commit());
+  }
 
-    @Tag("exception")
-    @Test
-    @DisplayName("test case for test throw exact exception")
-    void test_exception_exactly() {
-        assertThrows(
-                Exception.class,
-                () -> api.commit());
-    }
+  @TestFactory
+  @DisplayName("dynamic test !")
+  Stream<DynamicTest> dynamicTesting() {
+    return Stream.of(
+        DynamicTest.dynamicTest(
+            "test",
+            () -> {
+              assertNotNull(api.getName());
+              assertTrue(api.validate());
+              assertEquals(1, "".split(",").length);
+            }));
+  }
 }
